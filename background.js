33d615c401911a55c14b358e5ab36af6c6f846ee
lapayx <lapayx@mail.ru>
2019-06-11 09:38:59 +0300

    v2.3

--- background.js
+++ background.js
@@ -1,36 +1,180 @@
-var b  =  typeof(browser) != "undefined" ? browser : chrome;
+var brows  =  typeof(browser) != "undefined" ? browser : chrome;
 
 function checkURL(tabID, changeInfo, tab){
 	// If it satisfies the criteria (the URL containing 'www.opera.com')
     //b.pageAction.show(tabID);
 	if (tab.url.indexOf('vk') > -1) {
 		// Shows the page action
-		b.pageAction.show(tabID);
+		brows.pageAction.show(tabID);
 	}
 }
-b.tabs.onUpdated.addListener(checkURL);
+brows.tabs.onUpdated.addListener(checkURL);
 
 
 // отправка
 //chrome.extension.sendMessage({type: "render_output", msg: request.msg});
 // слушатель
-b.runtime.onMessage.addListener(function (request, sender, sendResponse)
+brows.runtime.onMessage.addListener(function (request, sender, sendResponse)
 {
 
 //    console.log(request);
-  switch (request.type) {
-    case "request_count_hiden_posts":
-    {
-      let count = localStorage.getItem("countHidePost") | 0;
-      b.runtime.sendMessage({type: "response_count_hiden_posts", count:count});
-      break;
-  }
-      case "inc_hiden_posts":
-      {
-        let count = localStorage.getItem("countHidePost") | 0;
-        localStorage.setItem("countHidePost",count +1);
+	switch (request.type) {
+		case "request_count_hiden_posts":
+		{
+			let count = localStorage.getItem("countHidePost") || 0;
+			brows.runtime.sendMessage({type: "response_count_hiden_posts", count:count});
+			break;
+		}
+		case "get_storage":
+		{
+			let postsRaw = localStorage.getItem("HidePost") || "[]";
+			let posts = JSON.parse(postsRaw);
+			
+			if(!!request.tab){
+			
+				brows.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				  chrome.tabs.sendMessage(tabs[0].id, {type: "response_get_storage", posts:posts});
+				});
+			} else {
+				brows.runtime.sendMessage({type: "response_get_storage", posts:posts});
+			}
 
-        break;
-        }
+			break;
+		}
+		case "inc_hiden_posts":
+		{
+			let count = parseInt(localStorage.getItem("countHidePost") || 0);
+			localStorage.setItem("countHidePost",count +1);
+			break;
+		}
+		case "delete_hiden_posts":
+		{
+			let postsRaw = localStorage.getItem("HidePost") || "[]";
+			let old = JSON.parse(postsRaw);
+			let posts = [];
+			for(let i = 0; i < old.length; i++){
+				if(i == request.id )
+					continue;
+				posts.push(old[i]);
+			};
+			localStorage.setItem("HidePost",JSON.stringify(posts));
+			
+		
+			
+			brows.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			  chrome.tabs.sendMessage(tabs[0].id, {type: "response_get_storage", posts:posts});
+			});
+	
+			brows.runtime.sendMessage({type: "response_get_storage", posts:posts});
+			
+			break;
+		}
+		case "add_hiden_posts":
+		{
+			let postsRaw = localStorage.getItem("HidePost") || "[]";
+			let posts = JSON.parse(postsRaw);
+			posts.push(request.text);
+			localStorage.setItem("HidePost",JSON.stringify(posts));
+			
+		
+			
+			brows.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			  chrome.tabs.sendMessage(tabs[0].id, {type: "response_get_storage", posts:posts});
+			});
+	
+			brows.runtime.sendMessage({type: "response_get_storage", posts:posts});
+			
+			break;
+		}
+	}
+});
+
+
+brows.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+		if (!localStorage.getItem("HidePost")){
+			let posts = [
+				"Что делают новички, когда их бизнес с Китаем только стартовал?",
+				"Обучаем веб-дизайну с нуля до профи за 2 месяца. Даже если вы до этого",
+				"Кожаное портмоне Wild Alligator со скидкой -50%",
+				"Умеешь играть в покер и сидишь без денег?",
+				"дравствуйте, мои дорогие! Меня зовут Валентина Иванова.",
+				"Хочешь иметь стабильный доход",
+				"Согласно исследованиям журнала Форбс, трейдер - это самая",
+				"Девочки, обязательно сохраните себе этот пост!",
+				"В этом видео вы узнаете о 5 фишках, которые помогут вам",
+				"Хочешь узнать, как я в 25 лет заработал на Ferrari за 3 мес",
+				"цветов - 6 неповторимых образов",
+				"Торговля Китайскими товарами через интернет магазин по",
+				"Более 300 реальных фото отзывов",
+				"Представляете себе эту сумму",
+				"расскажем о бесплатных методах продвижения",
+				"Яркие и стильные Leggings",
+				"Распродажа парфюма",
+				"Сидишь без денег?",
+				"Любимые часы кинозвезд!",
+				"Портмоне вызывающе уважение",
+				"инвестор",
+				"Олега Карнауха",
+				"yквaльно мeсяц назaд со мной",
+				"вкусный подарок для вашей любимой девочки",
+				"КАК НАЧАТЬ СВОЙ БИЗНЕС, НЕ ИМЕЯ ОПЫТА",
+				"азговоры о косметике, нижнем белье",
+				"начала меня уволили с работы",
+				"на счeт в 2 раза бoльшe",
+				"дома в интернете зарабатывать",
+				"долг перевалил за",
+				"Внес депозит, получил",
+				"хитрых трюка для запуска интернет бизнеса",
+				"модели открытия бизнеса",
+				"рублей за день!"
+			];
+			localStorage.setItem("HidePost",JSON.stringify(posts));
+		}
+        
+    }else if(details.reason == "update"){
+        var thisVersion = brows.runtime.getManifest().version;
+       // console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
     }
+	
+	if (!localStorage.getItem("HidePost")){
+			let posts = [
+				"Что делают новички, когда их бизнес с Китаем только стартовал?",
+				"Обучаем веб-дизайну с нуля до профи за 2 месяца. Даже если вы до этого",
+				"Кожаное портмоне Wild Alligator со скидкой -50%",
+				"Умеешь играть в покер и сидишь без денег?",
+				"дравствуйте, мои дорогие! Меня зовут Валентина Иванова.",
+				"Хочешь иметь стабильный доход",
+				"Согласно исследованиям журнала Форбс, трейдер - это самая",
+				"Девочки, обязательно сохраните себе этот пост!",
+				"В этом видео вы узнаете о 5 фишках, которые помогут вам",
+				"Хочешь узнать, как я в 25 лет заработал на Ferrari за 3 мес",
+				"цветов - 6 неповторимых образов",
+				"Торговля Китайскими товарами через интернет магазин по",
+				"Более 300 реальных фото отзывов",
+				"Представляете себе эту сумму",
+				"расскажем о бесплатных методах продвижения",
+				"Яркие и стильные Leggings",
+				"Распродажа парфюма",
+				"Сидишь без денег?",
+				"Любимые часы кинозвезд!",
+				"Портмоне вызывающе уважение",
+				"инвестор",
+				"Олега Карнауха",
+				"yквaльно мeсяц назaд со мной",
+				"вкусный подарок для вашей любимой девочки",
+				"КАК НАЧАТЬ СВОЙ БИЗНЕС, НЕ ИМЕЯ ОПЫТА",
+				"азговоры о косметике, нижнем белье",
+				"начала меня уволили с работы",
+				"на счeт в 2 раза бoльшe",
+				"дома в интернете зарабатывать",
+				"долг перевалил за",
+				"Внес депозит, получил",
+				"хитрых трюка для запуска интернет бизнеса",
+				"модели открытия бизнеса",
+				"рублей за день!"
+			];
+			localStorage.setItem("HidePost",JSON.stringify(posts));
+		}
+		
 });
--- popup.js
+++ popup.js
@@ -3,17 +3,57 @@ var b  =  typeof(browser) != "undefined" ? browser : chrome;
 
 // отправка
 b.runtime.sendMessage({type: "request_count_hiden_posts"});
+b.runtime.sendMessage({type: "get_storage"});
 // слушатель
 b.runtime.onMessage.addListener(function (request, sender, sendResponse)
 {
   switch (request.type) {
-    case "response_count_hiden_posts":
-        var elem = document.getElementById('count');
-        var count = localStorage.getItem("countHidePost");
-        elem.textContent = parseInt(request.count||0);
+
+    case "response_count_hiden_posts":{
+	
+        let elem = document.getElementById('count');
+        elem.textContent = parseInt(request.count);
       break;
+	}
+	  case "response_get_storage":
+	  {
+
+        let elem = document.getElementById('post');
+		while(elem.children.length>0){
+		
+			elem.children[0].remove();
+		  }
+        for(let i = 0; i<request.posts.length;i++){
+			let item =document.createElement("p");
+			item.textContent = request.posts[i];
+			let del = document.createElement("button");
+			del.textContent="X";
+			del.onclick = function(){
+				//alert(i);
+				b.runtime.sendMessage({type: "delete_hiden_posts",id:i});
+			};
+			item.appendChild(del);
+			elem.appendChild(item);
+		}
+      break;
+	  }
   }
+  
+  
+let newPostBut = document.getElementById('buttonNewPost');
+newPostBut.onclick = function(){
+	let val = document.getElementById('inputNewPost').value.trim();
+	if(val.length <4){
+		alert("Короткое имя, min 5");
+		return;
+	}
+	b.runtime.sendMessage({type: "add_hiden_posts",text:val});
+	document.getElementById('inputNewPost').value = "";
+}
+
+
 });
+
 /*
 
 
