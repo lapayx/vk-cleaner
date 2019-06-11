19de2ca897ffc615b7062767fc8f9f55056df041
lapayx <lapayx@mail.ru>
2019-06-11 10:57:52 +0300

    change style popup

--- popup.js
+++ popup.js
@@ -22,17 +22,36 @@ b.runtime.onMessage.addListener(function (request, sender, sendResponse)
 		while(elem.children.length>0){
 		
 			elem.children[0].remove();
-		  }
+      }
+      
+      /*
+       <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                        </tr>*/
+
         for(let i = 0; i<request.posts.length;i++){
-			let item =document.createElement("p");
-			item.textContent = request.posts[i];
-			let del = document.createElement("button");
+      let item =document.createElement("tr");
+      let d1 = document.createElement("td");
+      d1.textContent = request.posts[i];
+      
+      let d2 = document.createElement("td");
+       d2.classList.add("text-center");
+       d2.classList.add("align-middle");
+      //d2.textContent = request.posts[i];
+//<button type="button" class="btn btn-dark">Dark</button>
+      let del = document.createElement("button");
+      del.classList.add('btn');
+      del.classList.add('btn-danger')
+      del.classList.add('btn-sm')
 			del.textContent="X";
 			del.onclick = function(){
 				//alert(i);
 				b.runtime.sendMessage({type: "delete_hiden_posts",id:i});
 			};
-			item.appendChild(del);
+      d2.appendChild(del);
+      item.appendChild(d1);
+      item.appendChild(d2);
 			elem.appendChild(item);
 		}
       break;
